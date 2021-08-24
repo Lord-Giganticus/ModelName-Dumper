176a796cfe44fc23f4e8ed1a2bbef763288ecb4b
Lord-Giganticus <mrubins64@gmail.com>
2021-08-24 11:47:57 -0600

    Add a counter and improve the script

--- Byml_Dumper.py
+++ Byml_Dumper.py
@@ -10,23 +10,30 @@ if os.getcwd() is not homedir:
 
 files = glob("*.szs")
 
-Sarc_cs = glob("*\\*.exe")[2]
+Sarc_cs = glob("*\\*.exe")[3]
 
 for file in files:
     subprocess.call(f'{Sarc_cs} {file}')
 
-files = glob("*.byml")
+    files = glob("*.byml")
 
-Byml_cs = glob("*\\*.exe")[0]
+    Byml_cs = glob("*\\*.exe")[0]
 
-for file in files:
-    subprocess.call(f'{Byml_cs} {file}')
-    os.remove(file)
+    for file in files:
+        subprocess.call(f'{Byml_cs} {file}')
+        os.remove(file)
 
-Dumper = glob("*\\*.exe")[1]
+    Dumper = glob("*\\*.exe")[2]
 
-files = glob("*.yml")
+    files = glob("*.yml")
 
-for file in files:
-    subprocess.call(f'{Dumper} {file}')
-    os.remove(file)
+    for file in files:
+        subprocess.call(f'{Dumper} {file}')
+        os.remove(file)
+
+    Counter = glob('*\\*.exe')[1]
+
+    files = glob("*.txt")
+
+    for file in files:
+        subprocess.call(f'{Counter} {file}')
---
+++ Source/Counter/ModelName_Counter/ModelName_Counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ModelName_Counter
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length is 0) throw new ArgumentException();
+            var filelist = new List<FileInfo>();
+            foreach (var arg in args.Where(s => new FileInfo(s).Exists && new FileInfo(s).Extension is ".txt"))
+            {
+                filelist.Add(new FileInfo(arg));
+            }
+            foreach (var file in filelist)
+            {
+                var dict = new Dictionary<string, int>();
+                foreach (var line in File.ReadAllLines(file.FullName))
+                    if (!dict.ContainsKey(line) && !string.IsNullOrWhiteSpace(line))
+                        dict.Add(line, File.ReadAllLines(file.FullName).Where(s => s == line).Count());
+                var writer = new StreamWriter($"{file.Directory.FullName}\\{Path.GetFileNameWithoutExtension(file.FullName)}.count.txt");
+                foreach (var d in dict)
+                    writer.WriteLine($"The number of times {d.Key} appeared: {d.Value}");
+                writer.Close();
+            }
+        }
+    }
+}
