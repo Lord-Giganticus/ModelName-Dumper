c2fb5fc682090fcb34d30f0057bb177cf8f3f2fd
Lord-Giganticus <mrubins64@gmail.com>
2021-08-24 09:36:52 -0600

    Add source code, and add a switch level to test

---
+++ Source/Dumper/ModelName.Dumper/ModelName.Dumper/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace ModelName.Dumper
+{
+    static class Program
+    {
+        static string AppLocation => new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
+
+        static void Main(string[] args)
+        {
+            if (args.Length is 0) throw new ArgumentException();
+            var files = new List<FileInfo>();
+            foreach (var arg in args)
+            {
+                files.Add(new FileInfo(arg));
+            }
+            foreach (var file in files)
+            {
+                string endian;
+                switch (File.ReadAllLines(file.FullName)[1].Substring(File.ReadAllLines(file.FullName)[1].IndexOf(":") + 2))
+                {
+                    case "True":
+                        endian = "WiiU";
+                        break;
+                    case "False":
+                        endian = "Switch";
+                        break;
+                    default:
+                        endian = null;
+                        break;
+                }
+                var names = new List<string>();
+                if (file.Extension is ".yml")
+                {
+                    foreach (var line in File.ReadAllLines(file.FullName))
+                    {
+                        if (line.Contains("ModelName"))
+                        {
+                            if (line.Substring(line.IndexOf(':') + 2) != "null")
+                            {
+                                names.Add(line.Substring(line.IndexOf(':') + 2));
+                            }
+                        }
+                    }
+                }
+                Directory.SetCurrentDirectory(file.Directory.FullName);
+                File.WriteAllLines($"{file.Name.ReverseSubstring(file.Name.IndexOf('.'))}-{endian ?? "unknown"}.txt", names.ToArray());
+                Directory.SetCurrentDirectory(AppLocation);
+            }
+        }
+
+        static string ReverseSubstring(this string Str, int index)
+        {
+            List<string> arr()
+            {
+                var r = new List<string>();
+                for (int i = 0; i < index; i++)
+                {
+                    r.Add(Str[i].ToString());
+                }
+                return r;
+            };
+            return string.Join(string.Empty, arr().ToArray());
+        }
+    }
+}
---
+++ Source/Sarc.cs/Sarc.Dumper/Sarc.Dumper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SARCExt;
+using Hack.io.YAZ0;
+
+namespace Sarc.Dumper
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var home_dir = Directory.GetCurrentDirectory();
+            if (args.Length is 0) throw new ArgumentException();
+            foreach (var arg in args)
+            {
+                if (new FileInfo(arg).Exists && new FileInfo(arg).Extension is ".szs")
+                {
+                    var SarcData = SARC.UnpackRamN(YAZ0.Decompress(File.ReadAllBytes(arg)));
+                    foreach (var f in SarcData.Files)
+                    {
+                        if (f.Key.EndsWith(".byml"))
+                        {
+                            if (f.Key.ReverseSubstring(f.Key.IndexOf(".")).EndsWith("Map"))
+                            {
+                                Directory.SetCurrentDirectory(new FileInfo(arg).Directory.FullName);
+                                File.WriteAllBytes(f.Key, f.Value);
+                            }
+                        }
+                        Directory.SetCurrentDirectory(home_dir);
+                    }
+                }
+            }
+        }
+
+        static string ReverseSubstring(this string Str, int index)
+        {
+            var arr = new List<string>();
+            for (int i = 0; i < index; i++)
+            {
+                arr.Add(Str[i].ToString());
+            }
+            return string.Join(string.Empty, arr.ToArray());
+        }
+    }
+}
